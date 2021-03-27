9e4c73cc944211b17667f56d46495bfa46bf442f
Cristian Giraldo <cristian.giraldo@live.com>
2021-03-27 12:19:34 -0500

    Added log config

--- src/main/java/org/acme/GreetingResource.java
+++ src/main/java/org/acme/GreetingResource.java
@@ -1,5 +1,6 @@
 package org.acme;
 
+import org.jboss.logging.Logger;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
@@ -8,9 +9,15 @@ import javax.ws.rs.core.MediaType;
 @Path("/hello-resteasy")
 public class GreetingResource {
 
+    private static final Logger log = Logger.getLogger(GreetingResource.class);
+
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
+        log.info("info");
+        log.debug("debug");
+        log.info(log.isDebugEnabled());
+        log.debug("cristian");
         return "Hello RESTEasy";
     }
 }
